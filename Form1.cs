dcc18e420d1fc52f062cc64ded325871fbaf3bb3
Lea Ann Bradford <leaann@notch8.com>
2020-05-03 15:56:23 -0700

    implement  parallel for each and cancel tasks

--- Form1.cs
+++ Form1.cs
@@ -15,7 +15,9 @@ namespace Lab4
 	public partial class Form1 : Form
 	{
 		#region Fields
-		
+
+		CancellationTokenSource m_TokenSource = null;
+
 		#endregion Fields
 
 		#region Constructors
@@ -32,7 +34,14 @@ namespace Lab4
 		/// <param name="pi">New value for pi</param>
 		private void UpdatePiTextBlock(string pi)
 		{
-			// TODO:
+			if (piTextBox.InvokeRequired)
+			{
+				piTextBox.BeginInvoke(new Action<string>(UpdatePiTextBlock), pi);
+			}
+			else
+			{
+				piTextBox.Text = pi;
+			}
 		}
 		
 		/// <summary>
@@ -40,7 +49,30 @@ namespace Lab4
 		/// </summary>
 		private void CalculatePiTask()
 		{
-			// TODO:
+			try
+			{
+				for (int i = 2; i < 10000000; i++)
+				{
+					m_TokenSource.Token.ThrowIfCancellationRequested();
+					DateTime start = DateTime.Now;
+
+					string pi = MathStuff.Calculate(i);
+					UpdatePiTextBlock(pi);
+
+					DateTime end = DateTime.Now;
+
+					double totalMs = end.Subtract(start).TotalMilliseconds;
+					if (totalMs < 500)
+					{
+						Thread.Sleep(500 - (int)totalMs);
+					}
+				}
+			}
+			catch
+			{
+				Console.WriteLine("Cancelled.");
+				throw;
+			}
 		}
 		#endregion Methods
 
@@ -52,7 +84,25 @@ namespace Lab4
 		/// <param name="e"></param>
 		private void parallelGoButton_Click(object sender, EventArgs e)
 		{
-			// TODO:
+			var query = from Control child in parallelGroup.Controls
+						where child is ProgressBar
+						select child as ProgressBar;
+			
+			Action<ProgressBar, int> updateProgressBarValue = delegate (ProgressBar pb, int value)
+			{
+				pb.Value = value;
+			};
+
+			Parallel.ForEach(query, progressBar =>
+			{
+				for (int i = 0; i < progressBar.Maximum; i++)
+				{
+					if (i % 100 == 0)
+					{
+						progressBar.BeginInvoke(updateProgressBarValue, progressBar, i);
+					}
+				}
+			});
 		}
 
 		/// <summary>
@@ -62,12 +112,42 @@ namespace Lab4
 		/// <param name="e"></param>
 		private void taskGoButton_Click(object sender, EventArgs e)
 		{
-			// TODO:
+			taskGoButton.Enabled = false;
+			taskCancelButton.Enabled = true;
+
+			m_TokenSource = new CancellationTokenSource();
+
+			var ui = TaskScheduler.FromCurrentSynchronizationContext();
+
+			Task calculatePiTask = Task.Factory.StartNew(CalculatePiTask, m_TokenSource.Token);
+
+			var resultOK = calculatePiTask.ContinueWith(resultTask =>
+			{
+				MessageBox.Show("Calculation finished", "Task Complete", MessageBoxButtons.OK, MessageBoxIcon.Information);
+				taskCancelButton.Enabled = false;
+				taskGoButton.Enabled = true;
+			},
+			CancellationToken.None,
+			TaskContinuationOptions.OnlyOnRanToCompletion,
+			ui);
+
+			var resultCancel = calculatePiTask.ContinueWith(resultTask =>
+			{
+				MessageBox.Show("Calculation stopeed by user", "Task Cancelled", MessageBoxButtons.OK, MessageBoxIcon.Information);
+				taskCancelButton.Enabled = false;
+				taskGoButton.Enabled = true;
+			},
+			CancellationToken.None,
+			TaskContinuationOptions.OnlyOnCanceled,
+			ui);
 		}
 
 		private void taskCancelButton_Click(object sender, EventArgs e)
 		{
-			// TODO:
+			if (m_TokenSource != null)
+			{
+				m_TokenSource.Cancel();
+			}
 		}
 
 		#endregion Event Handlers
